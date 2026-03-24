196325a8741b982bb4da5070b15528f91be89601
prathampaleriya <prathampaleriya@gmail.com>
2026-03-25 03:24:52 +0530

    fixed sarvam

--- app/services/voice_service/servam_client.py
+++ app/services/voice_service/servam_client.py
@@ -2,7 +2,6 @@ from sarvamai import SarvamAI
 from app.utils.env_load import sarvam_api
 
 sarvam_client = SarvamAI(
-    base_url="https://api.sarvam.ai/",
     api_subscription_key=sarvam_api,
 )
 
