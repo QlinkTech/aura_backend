1b8aae94caf99dcba9269e91cb01e67803822c08
prathampaleriya <prathampaleriya@gmail.com>
2026-03-27 22:18:10 +0530

    updated profile

--- app/routes/user_sub_routes/profile.py
+++ app/routes/user_sub_routes/profile.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from app.services.auth_service import get_active_user
+from app.services.auth_service import get_active_user, get_current_user
 from app.services.db.user_profile_utils import get_user_details
 from app.utils.logger_config import logger
 
@@ -7,7 +7,7 @@ profile_router = APIRouter()
 
 
 @profile_router.get("/user-profile")
-def get_user(current_user=Depends(get_active_user)):
+def get_user(current_user=Depends(get_current_user)):
     email = current_user["email"]
     logger.info("Get user profile request", extra={"email": email})
     return get_user_details(email=email)
